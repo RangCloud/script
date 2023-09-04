8194c7a0fb4f0807f1aed76f58d9df2dae1098f1
RangCloud <mtr008@naver.com>
2023-09-04 09:39:41 +0900

    230904

---
+++ 26다음/nightdaybtn2.js
@@ -0,0 +1,38 @@
+let Links = {
+  setColor: function (color) {
+    /*  var aList = document.querySelectorAll("a");
+    var i = 0;
+    while (i < aList.length) {
+      aList[i].style.color = color;
+      i = i + 1;
+    } */
+    $("a").css("color", color);
+  },
+};
+
+let Body = {
+  setColor: function (color) {
+    /* document.querySelector("body").style.color = color; */
+    $("body").css("color", color);
+  },
+  setBackgroundColor: function (color) {
+    /* document.querySelector("body").style.backgroundColor = color; */
+    $("body").css("backgroundColor", color);
+  },
+};
+function nightDayHandler(self) {
+  var target = document.querySelector("body");
+  if (self.value === "night") {
+    Body.setBackgroundColor("black");
+    Body.setColor("white");
+    self.value = "day";
+
+    Links.setColor("powderblue");
+  } else {
+    Body.setBackgroundColor("white");
+    Body.setColor("black");
+    self.value = "night";
+
+    Links.setColor("blue");
+  }
+}
